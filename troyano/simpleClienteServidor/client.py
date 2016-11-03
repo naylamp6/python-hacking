51e09f4287ccd5e601f51852ef45db278cd1b9e3
archroot <balon-cli@protonmail.com>
2016-11-04 00:22:11 +0100

    Create client.py

---
+++ troyano/simpleClienteServidor/client.py
@@ -0,0 +1,10 @@
+import socket
+
+servidor = "127.0.0.1" # La IP a conectarse
+puerto = 5555 # El mismo puerto que el servidor, obviamente
+
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cliente.connect((servidor, puerto))
+cliente.send("HOLA SERVIDOR");
+respuesta = cliente.recv(4096)
+print respuesta
